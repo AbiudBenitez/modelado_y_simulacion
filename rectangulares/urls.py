e6f666774755e877aa4f798d7e7272b6d9269f95
abiudbenitez <abiudmisael10@hotmail.com>
2024-09-10 23:54:33 -0600

    Se añadieron urls de las nuevas vistas

--- rectangulares/urls.py
+++ rectangulares/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("proyects/", views.proyecta),
     path("task/<int:id>", views.task),
     path("tasks/rectangulares_mixto/", views.rectangulares_mixto, name="rectangulares_mixto"),
-    path("tasks/recValue/", views.recValue, name="recValue")
+    path("tasks/recValue/", views.recValue, name="recValue"),
+    #SE DECLARAN LOS URLS QUE SOLO PROCESAN DATOS
+    path("mixtos_datos/", views.mixtos_datos, name="mixtos_datos"),
 ]
