521bb977ff1a852dd26671a5088ec951cae67892
abiudbenitez <abiudmisael10@hotmail.com>
2024-09-12 00:05:02 -0600

    Se cambio el tipo de campo para que solo acepte numeros

--- rectangulares/forms.py
+++ rectangulares/forms.py
@@ -1,13 +1,28 @@
-from typing import Any, Mapping
+from typing import Any
 from django import forms
 from django.forms.renderers import BaseRenderer
 from django.forms.utils import ErrorList
 
 class datos(forms.Form):
-  a = forms.CharField(label="Introduce el valor de a", required=True)
-  Xo = forms.CharField(label="Introduce el valor de Xo", required=True)
-  C = forms.CharField(label="Introduce el valor de C", required=True)
-  m = forms.CharField(label="Introduce el valot de m", required=True)
+  a = forms.IntegerField(
+    label="Introduce el valor de a",
+    required=True
+  )
+  
+  Xo = forms.IntegerField(
+    label="Introduce el valor de Xo", 
+    required=True
+  )
+  
+  C = forms.IntegerField(
+    label="Introduce el valor de C", 
+    required=True
+  )
+  
+  m = forms.IntegerField(
+    label="Introduce el valor de m", 
+    required=True
+  )
 
   def __init__(self, *args, **kwargs):
     super(datos, self).__init__(*args, **kwargs)  
