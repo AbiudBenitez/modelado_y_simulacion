0dcfbc0df66a064801c5661a8a5afde531f8f68c
abiudbenitez <abiudmisael10@hotmail.com>
2024-09-12 00:08:00 -0600

    se cambiaron los links para adaptarlo al nuevo mod

--- rectangulares/urls.py
+++ rectangulares/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path("proyects/", views.proyecta),
     path("task/<int:id>", views.task),
     path("tasks/rectangulares_mixto/", views.rectangulares_mixto, name="rectangulares_mixto"),
-    path("tasks/recValue/", views.recValue, name="recValue"),
+    path("tasks/rectangulares_multi/", views.rectangulares_multi, name="rectangulares_multi"),
+    path("tasks/rectangulares_mixto/respuesta", views.recValue, name="recValue"),
+    path("tasks/rectangulares_multi/respuesta", views.recMultiValue, name="recMultiValue"),
     #SE DECLARAN LOS URLS QUE SOLO PROCESAN DATOS
     path("mixtos_datos/", views.mixtos_datos, name="mixtos_datos"),
+    path("multi_datos/", views.multi_datos, name="multi_datos"),
 ]
