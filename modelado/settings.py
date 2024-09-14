94a58d24408228bdfddd092e77bfdf34228d0270
abiudbenitez <abiudmisael10@hotmail.com>
2024-09-13 19:23:20 -0600

    Se añadieron mas archivos

--- modelado/settings.py
+++ modelado/settings.py
@@ -120,9 +120,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+# STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Heroku: Actualice la configuración de la base de datos desde $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Archivos estáticos (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+# La ruta absoluta al directorio donde Collectstatic recopilará archivos estáticos para su implementación.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# La URL que se utilizará cuando se haga referencia a archivos estáticos (desde donde se entregarán)
+STATIC_URL = '/static/'
